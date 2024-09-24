56b729f01f30d0aa40c1a27b70775f2a1f5f87af
John Paul Arriola <arriolajp7@gmail.com>
2024-09-24 19:23:34 -0400

    Added the Interactive Map requirement using Mapbox API.

--- TheRoute/app/(tabs)/index.tsx
+++ TheRoute/app/(tabs)/index.tsx
@@ -1,70 +1,54 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View} from "react-native";
+import Mapbox, {MapView} from "@rnmapbox/maps";
+//import 'react-native-gesture-handler';
+//import {NavigationContainer} from '@react-navigation/native';
+//import { createStackNavigator } from '@react-navigation/native-stack';
+//import Main from '../screens/Main';
+//import Another from '../screens/Another';
+import Config from "react-native-config";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+Mapbox.setAccessToken(Config.MAPBOX_ACCESS_TOKEN);
 
-export default function HomeScreen() {
+const App = () => {
+console.log(Config.MAPBOX_ACCESS_TOKEN);
+console.log("no");
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
-}
+         <View style={styles.page}>
+           <View style={styles.container}>
+             <Mapbox.MapView
+                 style={styles.map}
+                 styleURL="mapbox://styles/mapbox/outdoors-v12"
+             />
+           </View>
+         </View>
+       );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+
+  page: {
+    flex: 1,
+
+    justifyContent: 'center',
+
     alignItems: 'center',
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+
+  container: {
+    height: "100%",
+
+    width: "100%",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+
+  map: {
+    flex: 1,
   },
 });
+
+
+
+
+
