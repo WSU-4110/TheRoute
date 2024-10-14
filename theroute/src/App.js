a58e2316576016e1c666ed8ca00a2056a3ce5433
Aaron Morrison <aaronmorrison03@gmail.com>
2024-10-14 18:13:38 -0400

    Edited App.js using geocoder for the search bar and added some lines in the package-lock.json file from installing the NPM package

--- theroute/src/App.js
+++ theroute/src/App.js
@@ -1,35 +1,53 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import './App.css'; // Assuming you have some custom styles
+import { Geocoder } from "@mapbox/search-js-react";
 
 // Import Mapbox CSS
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const App = () => {
+export default function MapWithGeocoder() {
   const mapContainerRef = useRef(null);
+  const mapInstanceRef = useRef();
+  //const [, setMapLoaded] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
     // Set Mapbox access token
     mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_KEY;
 
     // Initialize Mapbox map
-    const map = new mapboxgl.Map({
+      mapInstanceRef.current = new mapboxgl.Map({
       container: mapContainerRef.current, // Ref to the map container div
       style: 'mapbox://styles/mapbox/outdoors-v12', // Your map style
       center: [-83.06680531, 42.35908111], // Starting position [lng, lat]
       zoom: 14, // Starting zoom
     });
 
+    
+
     // Clean up on unmount
-    return () => map.remove();
+    return () => mapInstanceRef.current.remove();
   }, []);
 
   return (
-    <div>
-      {/* Map Container */}
-      <div ref={mapContainerRef} style={{ width: '100%', height: '100vh' }} />
-    </div>
+    <>
+      <Geocoder
+        accessToken={process.env.REACT_APP_MAPBOX_ACCESS_KEY}
+        map={mapInstanceRef.current}
+        mapboxgl={mapboxgl}
+        value={inputValue}
+        onChange={(d) => {
+          setInputValue(d);
+        }}
+        marker
+      />
+      <div>
+        {/* Map Container */}
+        <div ref={mapContainerRef} style={{ width: '100%', height: '100vh' }} />
+      </div>
+    </>
   );
 };
 
-export default App;
+//export default App;
