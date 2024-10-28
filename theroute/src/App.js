8967e1f0a8f847264f8d9c07da051e6454a6f8dd
dylanlewis12 <hi4491@wayne.edu>
2024-10-28 12:20:38 -0400

    Update App.js
    Removing user route

--- theroute/src/App.js
+++ theroute/src/App.js
@@ -12,7 +12,6 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/user" element={<UserApp />} />
       </Routes>
     </Router>
   );
