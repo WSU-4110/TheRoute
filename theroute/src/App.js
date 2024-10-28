5179f83f889f33457fc72dbb4b7aef7ad315ce57
dylanlewis12 <hi4491@wayne.edu>
2024-10-28 12:20:49 -0400

    Update App.js

--- theroute/src/App.js
+++ theroute/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
-import UserApp from './components/UserApp';
 
 const App = () => {
   return (
