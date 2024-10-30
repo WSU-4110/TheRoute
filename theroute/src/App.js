a328b7a7747daea04817a2f455527d4be7e44560
John Paul Arriola <arriolajp7@gmail.com>
2024-10-30 15:06:40 -0400

    homework commit

--- theroute/src/App.js
+++ theroute/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Map from './pages/Map';
 import {Setup} from './pages/Setup';
+import './styles/App.css';
 
 const App = () => {
   return (
     <Router>
+      <nav className="navbar">
+        <div className="buttons">
+          <Link to="/map">Maps</Link>
+          <Link to="/setup">Trips</Link>
+        </div>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -16,6 +23,7 @@ const App = () => {
         <Route path="/map" element={<Map />} />
         <Route path="/setup" element={<Setup/>}/>
       </Routes>
+      
     </Router>
   );
 };
