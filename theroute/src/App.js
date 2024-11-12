9360a0b5c83784b04c200c5527f01c5eb8cd4f05
Dylan Lewis <hi4491@wayne.edu>
2024-11-12 16:27:15 -0500

    Added viewtrips route to App.js

--- theroute/src/App.js
+++ theroute/src/App.js
@@ -4,9 +4,15 @@ import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Map from './pages/Map';
+<<<<<<< HEAD
 import { Setup } from './pages/Setup';
 import AddExpense from './components/AddExpenses';
 import ViewExpense from './components/ViewExpenses';
+=======
+import {Setup} from "./pages/Setup";
+import AddExpense from "./components/AddExpenses";
+import ViewExpense from "./components/ViewExpenses";
+>>>>>>> 03be9f4 (Added viewtrips route to App.js)
 import Sidebar from './components/SideBar';
 import ViewTrips from './pages/ViewTrips';
 
@@ -19,10 +25,17 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/map" element={<Map />} />
+<<<<<<< HEAD
         <Route path="/setup" element={<Setup />} />
         <Route path="/add-expense" element={<AddExpense />} />
         <Route path="/view-expense" element={<ViewExpense />} />
         <Route path="/view-trips" element={<ViewTrips />} />
+=======
+        <Route path="/setup" element = {<Setup/>}/>
+        <Route path="/add-expense" element = {<AddExpense/>}/>
+        <Route path="/view-expense" element = {<ViewExpense/>}/>
+        <Route path="/view-trips" element = {<ViewTrips/>}/>
+>>>>>>> 03be9f4 (Added viewtrips route to App.js)
       </Routes>
     </Router>
   );
