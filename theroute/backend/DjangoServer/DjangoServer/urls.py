d35b001a99873436f00ac9e570546ea4de7d8639
Tawheed Mokammel <nirjhortawheed@gmail.com>
2024-11-19 18:46:51 -0500

    .

--- theroute/backend/DjangoServer/DjangoServer/urls.py
+++ theroute/backend/DjangoServer/DjangoServer/urls.py
@@ -7,13 +7,10 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView
 )
-<<<<<<< HEAD
 from userAPI import views
 from userExpenses.views import ExpenseView
 from userTrips.views import TripDetailsViewSet  # Import the TripDetails viewset
-=======
 from django.contrib import admin
->>>>>>> 143f6347e11bb5e25725f00da21be994d4d01e6e
 
 # Create a router to automatically handle routes for expenses
 router = DefaultRouter()
@@ -32,14 +29,12 @@ urlpatterns = [
     path('api/user/', user_views.UserView.as_view(), name='user'),
 
     # JWT token management endpoints
-<<<<<<< HEAD
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Obtain tokens
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Refresh access token
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),  # Verify access token
     
     # User expenses and trips: add, delete, get
     path('api/', include(router.urls)),  # API routes for expenses and trips
-=======
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
@@ -52,5 +47,4 @@ urlpatterns = [
     path('api/achievements/details/<int:achievement_id>/', user_views.achievement_details, name='achievement_details'),
     path('api/achievements/award/<str:achievement_key>/', user_views.award_achievement_view, name='award_achievement'),
     path('api/achievements/all/', user_views.list_all_achievements, name='list_all_achievements'),  # Added endpoint
->>>>>>> 143f6347e11bb5e25725f00da21be994d4d01e6e
 ]
